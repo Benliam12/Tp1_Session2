e102b3c56fd85d6e3e2068ec775b4cc00cbceda0
Benliam12 <s100live.ca@live.ca>
2019-02-26 11:23:10 -0500

    Putting some comments into Fiche class

--- TP1-Project/TP1-Project/Fiche.cpp
+++ TP1-Project/TP1-Project/Fiche.cpp
@@ -1,14 +1,116 @@
+/*==================================================================================================
+ * Auteur: William D'Anjou
+ * Laboratoire : TP1
+ * Fichier: Fiche.cpp 
+ * But: La classe Fiche sert a contenir les informations sur un score qui sera attribue a un joueur
+ * Date: 26 fevrier 2019
+*===================================================================================================*/
+
+//Inclusion des dependances
 #include <iostream>
 #include "Fiche.h"
 
+// #####################################################
+// ############ Getters de la classe ###################
+// #####################################################
 
-void Fiche::show(std::ostream& flux)
+/**
+ * Retourne le nombre de buts de la fiche
+ * 
+ * @return Nombre de buts
+ */
+int Fiche::getNbButs() const
 {
-	flux << std::endl << " PJ " << " B " << " P " << std::endl;
-	flux << " " << this->PJ << " " << this->buts << " " << this->passes;
+	return this->buts;
 }
 
-//Constructeurs de la classe fiche
+/**
+ * Retourne le nombre de passes de la fiche
+ *
+ * @return Nombre de passes
+ */
+int Fiche::getNbPasse() const
+{
+	this->passes;
+}
+
+/**
+ * Retourne le PJ de la fiche
+ *
+ * @return PJ
+ */
+int Fiche::getPJ() const
+{
+	return this->PJ;
+}
+
+// #####################################################
+// ############ Setters de la classe ###################
+// #####################################################
+
+/**
+ * Defini le nombre de buts de la fiche
+ * 
+ * @param buts Nombre de buts
+ */
+void Fiche::setButs(int buts)
+{
+	if(buts >= 0) // Le nombre de buts se doit d'etre positif
+	{
+		this->buts = buts;
+	}
+}
+
+/**
+ * Defini le nombre de passe de la fiche
+ * 
+ * @param passes Nombre de passes
+ */
+void Fiche::setPasses(int passes)
+{
+	if(passes >= 0) // Le nombre de passe doit etre positif
+	{ 
+		this->passes = passes;
+	}
+}
+
+/**
+ * Defini le PJ de la fiche
+ * 
+ * @param PJ
+ */
+void Fiche::setPJ(int PJ)
+{
+	if(PJ >= 0) // Le PJ doit etre positif
+	{
+		this->PJ = PJ;
+	}
+}
+
+// #####################################################
+// ######## Method sans retour de la classe ############
+// #####################################################
+
+/**
+ * Affiche l'objet fiche suivant un certain format d'affichage.
+ * 
+ * @param flux Le flux qui sera affiche
+ */
+void Fiche::show(std::ostream& flux)
+{
+	flux << std::endl << " PJ " << " B " << " P " << std::endl; // En-tete
+	flux << " " << this->PJ << " " << this->buts << " " << this->passes; // Score
+}
+
+// #####################################################
+// ############ Constructeurs de la classe #############
+// #####################################################
+
+/**
+ * Construit l'objet fiche a l'aide d'un autre fiche. Copie ses informations.
+ * 
+ * @param fiche Objet fiche a utiliser
+ */
 Fiche::Fiche(Fiche const& fiche)
 {
 	this->PJ = fiche.getPJ();
@@ -16,6 +118,13 @@ Fiche::Fiche(Fiche const& fiche)
 	this->passes = fiche.getNbPasse();
 }
 
+/**
+ * Construit l'objet fiche a l'aide de statistiques brutes.
+ * 
+ * @param PJ
+ * @param buts Nombre de buts marques
+ * @param passes Nombre de passes
+ */
 Fiche::Fiche(int PJ, int buts, int passes)
 {
 	this->PJ = PJ;
@@ -23,6 +132,9 @@ Fiche::Fiche(int PJ, int buts, int passes)
 	this->passes = passes;
 }
 
+/**
+ * Constructeur vide. Initialise les valeurs de PJ, buts et passes a 0.
+ */
 Fiche::Fiche()
 {
 	this->PJ = 0;
@@ -30,8 +142,13 @@ Fiche::Fiche()
 	this->passes = 0;
 }
 
+// #####################################################
+// ############ Destructeurs de la classe ##############
+// #####################################################
 
-//Destructeur de la classe Fiche
+/**
+ * Destructeur de la classe Fiche.
+ */
 Fiche::~Fiche()
 {
 	std::cout << "Destruction d'une fiche!" << std::endl;
